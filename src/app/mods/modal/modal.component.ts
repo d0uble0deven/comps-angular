ab4ab80583e9ee48a0cd230528a58e0feccafc4d
Dev Govindji <friendsofdev94@gmail.com>
2020-07-20 17:22:56 -0500

    renders modal correctly

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
-
-  constructor() { }
+  constructor(private el: ElementRef) {
+    console.log(this.el.nativeElement);
+  }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
 }
