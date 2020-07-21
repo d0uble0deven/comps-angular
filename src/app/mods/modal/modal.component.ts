eb47d0caa697990d154c6b9891c1324c86f79f31
Dev Govindji <friendsofdev94@gmail.com>
2020-07-21 15:08:14 -0500

    completes modal component

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,12 +6,26 @@ import { Component, OnInit, ElementRef } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+
+  @Output() close = new EventEmitter();
+
   constructor(private el: ElementRef) {
     console.log(this.el.nativeElement);
   }
 
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    // document.body.removeChild(this.el.nativeElement);
+    this.el.nativeElement.remove();
+
+  }
+
+  onCloseClick() {
+    this.close.emit()
+
   }
 
 }
